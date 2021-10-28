3b43e6a150c4efc60d4c4fe298c25cd9bad7cbb4
mubai <1186446929@qq.com>
2021-10-28 10:53:32 +0800

    ci(配置eslint和格式化工具): 设置好通用配置文件，后期将具体的参数及其说明文档完善好

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 
 <script>
 export default {
-  name: "App",
-  components: {},
+    name: "App",
+    components: {},
 };
 </script>
 
--- src/view/user/index.vue
+++ src/view/user/index.vue
@@ -36,13 +36,13 @@
 import { Row, Col, Icon, Cell, CellGroup } from 'vant';
 
 export default {
-  components: {
-    [Row.name]: Row,
-    [Col.name]: Col,
-    [Icon.name]: Icon,
-    [Cell.name]: Cell,
-    [CellGroup.name]: CellGroup
-  }
+    components: {
+        [Row.name]: Row,
+        [Col.name]: Col,
+        [Icon.name]: Icon,
+        [Cell.name]: Cell,
+        [CellGroup.name]: CellGroup
+    }
 };
 </script>
 
